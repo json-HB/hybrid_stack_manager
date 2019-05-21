1688b6a017373f051ad9ac3a8711eec5fd95041e
webyom <webyom@gmail.com>
2019-05-21 14:12:02 +0800

    0.8.0

--- ios/Classes/XURLRouter.h
+++ ios/Classes/XURLRouter.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #define kOpenUrlPrefix  @"hrd"
 
 typedef UIViewController* (^NativeOpenUrlHandler)(NSString *,NSDictionary *,NSDictionary *);
